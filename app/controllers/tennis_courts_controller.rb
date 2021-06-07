9c9be8090d89317407d4f1ebabc2d26df9a1bebc
이혜진 <4mikemendes@gmail.com>
2021-06-07 16:52:50 +0200

    create

--- app/controllers/tennis_courts_controller.rb
+++ app/controllers/tennis_courts_controller.rb
@@ -2,4 +2,25 @@ class TennisCourtsController < ApplicationController
   def index
     @tennis_courts = TennisCourts.all
   end
+
+  def new
+    @tennis_court = TennisCourt.new
+  end
+
+
+  def create
+    @tennis_court = TennisCourt.new(list_params)
+    if @tennis_court.save
+      redirect_to tennis_court_path(@tennis_court)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tennis_court_params
+    params.require(:tennis_court).permit(:court_name, :address, :price_per_hour, :description, :surface_type, :created_at, :updated_at)
+  end
+
 end
