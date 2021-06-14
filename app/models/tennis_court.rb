42ed65526869dee4fcee84f05cd57b363f2bac92
Marc della Faille <marcdellafaille@gmail.com>
2021-06-14 14:05:57 +0200

    dashboard back-end done

--- app/models/tennis_court.rb
+++ app/models/tennis_court.rb
@@ -4,7 +4,7 @@ class TennisCourt < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
   has_one_attached :photo
   has_many :schedules
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
   include PgSearch::Model
   pg_search_scope :search_by_court_name_and_address,
@@ -16,4 +16,14 @@ class TennisCourt < ApplicationRecord
   validates :address, presence: true
   validates :price_per_hour, presence: true
   validates :surface_type, presence: true
+
+  def total_booked_hours
+    total = 0
+    bookings.where(status: "accepted").where("beginning_date_time < ? ", DateTime.now).each do |booking|
+      booking_total = ((booking.end_date_time - booking.beginning_date_time) / (60 * 60)).to_i
+      p booking_total
+      total += booking_total
+    end
+    return total
+  end
 end
